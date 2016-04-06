f965fba28218ce73bccc236cb31ff688e1c66d69
Fredrik Eriksson <fegge22@hotmail.com>
2016-04-06 13:09:40 +0200

    Subtraktion, division och multiplikation har lagts till!

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,8 +1,20 @@
 
 public class Calculator {
 	
-	//Addition, add, +
+	//Addition, add, (+)
 	public double addition(double x, double y){
 		return x + y;
+	}
+	//Subtraktion, remove, (-)
+	public double subtraktion(double x, double y){
+		return x - y;
+	}
+	//Division, divide, (/)
+	public double division(double x, double y){
+		return x / y;
+	}
+	//Multiplication, multiply, (*)
+	public double multiplikation(double x, double y){
+		return x * y;
 	}
 }
